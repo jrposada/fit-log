6857f5c31a6edc8b987a8c3a6a993faeae14e8f2
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2025-02-13 22:26:43 +0100

    feat: frontend bucket

--- packages/frontend/src/ui/menu-button/menu-button-context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { MenuButtonService } from './menu-button-service';
-
-const requestContext = createContext<MenuButtonService | null>(null);
-
-export default requestContext;
