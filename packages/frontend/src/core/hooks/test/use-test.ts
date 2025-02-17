eedbbc8241a390a54b54c20b09baac9d63106f62
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2025-02-17 16:07:14 +0100

    feat: api calls from frontend

--- packages/frontend/src/core/hooks/test/use-test.ts
+++ packages/frontend/src/core/hooks/test/use-test.ts
@@ -5,8 +5,10 @@ export function useTest() {
   return useQuery({
     queryKey: ['test'],
     queryFn: async () => {
-      const response = await axios.get(`${process.env.VITE_API_BASE_URL}/test`);
-
+      console.log('url', import.meta.env.VITE_API_BASE_URL);
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_BASE_URL}/test`
+      );
       console.log(response);
       return response;
       // return JSON.parse(response) as Request[];
