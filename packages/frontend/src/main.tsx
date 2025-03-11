b61c682cc0a6152d028b832b280d6bdd8e7c662f
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2025-03-11 21:38:29 +0100

    chore: react query dev tools

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,6 +4,7 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SnackbarProvider } from 'notistack';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
@@ -23,6 +24,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <ThemeProvider theme={defaultTheme}>
         <CssBaseline />
         <SessionProvider>
