44d52cef27d78c7464660e8f0a2386454757bea0
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2025-11-23 13:32:16 +0100

    feat(app-mobile): add bottom tab navigation

--- packages/app-mobile/src/navigation/index.tsx
+++ packages/app-mobile/src/navigation/index.tsx
@@ -1,22 +1,20 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useVersion } from '@shared-react/api/version/use-version';
 import { StatusBar } from 'expo-status-bar';
 import { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
-import { StyleSheet, Text, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { StyleSheet, Text } from 'react-native';
 
 import BoulderEditorScreen from '../screens/boulder-editor-screen';
 import HomeScreen from '../screens/home-screen';
 import ImagePickerScreen from '../screens/image-picker-screen';
 import { RootStackParamList } from '../types/routes';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createBottomTabNavigator<RootStackParamList>();
 
 const Navigation: FunctionComponent = () => {
   const { t } = useTranslation();
-  const insets = useSafeAreaInsets();
   const { data: version } = useVersion();
 
   return (
@@ -32,6 +30,9 @@ const Navigation: FunctionComponent = () => {
           headerTitleStyle: {
             fontWeight: 'bold',
           },
+          headerRight: () => (
+            <Text style={styles.versionText}>{t('version', { version })}</Text>
+          ),
         }}
       >
         <Stack.Screen
@@ -50,35 +51,16 @@ const Navigation: FunctionComponent = () => {
           options={{ title: t('boulder.screen_title_editor') }}
         />
       </Stack.Navigator>
-
-      <View
-        style={{
-          ...styles.versionContainer,
-
-          paddingBottom: insets.bottom,
-          paddingLeft: insets.left,
-          paddingRight: insets.right,
-        }}
-      >
-        <Text style={styles.versionText}>{t('version', { version })}</Text>
-      </View>
     </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  versionContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    alignItems: 'center',
-    paddingVertical: 4,
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-  },
   versionText: {
-    fontSize: 10,
-    color: '#666',
+    fontSize: 11,
+    color: '#fff',
+    marginRight: 12,
+    opacity: 0.8,
   },
 });
 
