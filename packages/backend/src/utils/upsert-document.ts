a9715a6d4c1dc17e4730eaad31e26248d71de121
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2025-12-20 21:20:43 +0100

    fix(backend): upsert document null id

--- packages/backend/src/utils/upsert-document.ts
+++ packages/backend/src/utils/upsert-document.ts
@@ -1,17 +1,21 @@
-import { Document, Model } from 'mongoose';
+import { Document, Model, Types } from 'mongoose';
 
 export function upsertDocument<T extends Document>(
   model: Model<T>,
   id: string | undefined,
   data: Partial<T>
 ) {
+  const _id = id ? new Types.ObjectId(id) : new Types.ObjectId();
+
   return model
-    .findByIdAndUpdate(id, data, {
+    .findByIdAndUpdate(_id, data, {
       new: true,
       upsert: true,
       runValidators: true,
     })
     .orFail(
-      new Error(`${model.modelName} with ID "${id ?? ''}" could not be upsert.`)
+      new Error(
+        `${model.modelName} with ID "${_id ?? ''}" could not be upsert.`
+      )
     );
 }
