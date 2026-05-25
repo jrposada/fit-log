da7c7f315dfe356a878d679966a3f8d72c79aa6c
Javier Rodriguez Posada <j.rodriguez@rinkel.com>
2026-05-25 11:43:35 +0200

    feat(shared): remove ownership from training-session

--- packages/shared/src/models/training-session/training-session.ts
+++ packages/shared/src/models/training-session/training-session.ts
@@ -1,33 +1,28 @@
-import {
-  WithDepopulatedOwnership,
-  WithOwnership,
-} from '../auth/with-ownership.ts';
+import { WithDepopulatedOwnership } from '../auth/with-ownership.ts';
 import { Climb } from '../climb/climb.ts';
 import { Location } from '../location/location.ts';
 import { WithTimestamps } from '../utils/with-timestamps.ts';
 
-export type TrainingSession = WithOwnership<
-  WithTimestamps<{
-    /* Data */
-    id: string;
-    title: string;
-    notes?: string;
-    startedAt: string;
-    endedAt?: string;
-    lastActivityAt: string;
+export type TrainingSession = WithTimestamps<{
+  /* Data */
+  id: string;
+  title: string;
+  notes?: string;
+  startedAt: Date;
+  endedAt?: Date;
+  lastActivityAt?: Date;
 
-    /* References */
-    location?: Omit<WithDepopulatedOwnership<Location>, 'sectors'> & {
-      sectors: string[];
-    };
-    climbs: Omit<
-      WithDepopulatedOwnership<Climb>,
-      'image' | 'location' | 'sector'
-    > &
-      {
-        image: string;
-        location: string;
-        sector: string;
-      }[];
-  }>
->;
+  /* References */
+  location?: Omit<WithDepopulatedOwnership<Location>, 'sectors'> & {
+    sectors: string[];
+  };
+  climbs: Omit<
+    WithDepopulatedOwnership<Climb>,
+    'image' | 'location' | 'sector'
+  > &
+    {
+      image: string;
+      location: string;
+      sector: string;
+    }[];
+}>;
